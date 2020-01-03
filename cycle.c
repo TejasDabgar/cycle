4d4aa999900836637acd6efef9c15f3fe13881ab
TejasDabgar <47213113+TejasDabgar@users.noreply.github.com>
2020-01-03 18:18:11 +0530

    Add files via upload

---
+++ cycle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a;
+    printf("\n\n");
+    printf("      _\r\n");
+    printf("      \\\r\n");
+    printf("      /\\_\r\n");
+    printf("  ___/      ___\r\n");
+    printf(" /  _\\_____/_  \\\r\n");
+    printf(" \\___/     \\___/\r\n");
+    printf(" \n\n");
+ }
