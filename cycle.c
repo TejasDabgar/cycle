8e592ce68a7ab402e1ff50e736c1e18142def8d0
Tejas Dabgar <tejas.dabgar@softnautics.com>
2020-01-03 18:19:13 +0530

    Add check for length

--- cycle.c
+++ cycle.c
@@ -1,14 +1,30 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 int main(void)
 {
-	int a;
-    printf("\n\n");
-    printf("      _\r\n");
-    printf("      \\\r\n");
-    printf("      /\\_\r\n");
-    printf("  ___/      ___\r\n");
-    printf(" /  _\\_____/_  \\\r\n");
-    printf(" \\___/     \\___/\r\n");
-    printf(" \n\n");
+	int i,j;
+	printf(" \n\n");
+
+    char chr[6][200] ={"      _\r\n",
+		       "      \\\r\n",
+		       "      /\\_\r\n",
+		       "  ___/      ___\r\n",
+                       " /  _\\_____/_  \\\r\n",
+                       " \\___/     \\___/\r\n"};
+
+   for(i=0;i<6;i++)
+   {
+	for(j=0; j<200; j++)
+		printf("%c", chr[i][j]);
+   }
+   printf("strlen(chr[0]):%lu\n", strlen(chr[0]));
+   printf("strlen(chr[1]):%lu\n", strlen(chr[1]));
+   printf("strlen(chr[2]):%lu\n", strlen(chr[2]));
+   printf("strlen(chr[3]):%lu\n", strlen(chr[3]));
+   printf("strlen(chr[4]):%lu\n", strlen(chr[4]));
+   printf("strlen(chr[5]):%lu\n", strlen(chr[5]));
+
+printf(" \n\n");
  }
